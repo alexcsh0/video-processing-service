1140e56d94e53f77b86992f90708f608801163d3
Alex Chung <alexcssh27@gmail.com>
2025-05-21 11:14:16 -0700

    fix input validation and enhance video processing logic

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg"; 
+import path from "path";
 
 const app = express();
 app.use(express.json());
 
 app.post('/process-video', (req, res) => {
     // get path of input video file from the request body
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
+    const { inputFilePath, outputFilePath, height } = req.body;
 
-    console.log(`Input file path: ${inputFilePath}`);
     if (!inputFilePath) {
         return res.status(400).send("Bad Request: Missing input file path");
     }
@@ -17,8 +16,19 @@ app.post('/process-video', (req, res) => {
         return res.status(400).send("Bad Request: Missing output file path");
     }
 
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=trunc(oh*a/2)*2:360") // convert to 360p
+    const h = parseInt(height);
+    if (isNaN(h) || h < 144 || h > 2160) {
+        return res
+          .status(400)
+          .send("Bad Request: Height must be a number between 144 and 2160");
+    }
+
+    const inputPath = path.resolve(inputFilePath);
+    const outputPath = path.resolve(outputFilePath);
+    const scaleFilter = `scale=trunc(oh*a/2)*2:${h}`; // to specified height
+
+    ffmpeg(inputPath)
+        .outputOptions("-vf", scaleFilter) // convert to 360p
         .on("start", (commandLine: string) => {
             console.log(`FFmpeg process started with command: ${commandLine}`);
         })
@@ -30,7 +40,7 @@ app.post('/process-video', (req, res) => {
             console.log(`An error occurred: ${err.message}`);
             res.status(500).send(`Internal Server Error: ${err.message}`);
         })
-        .save(outputFilePath); // not adding a return because of this
+        .save(outputPath); // not adding a return because of this
     
     return;
 });

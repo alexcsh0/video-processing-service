835cd9749a268f23738766b13494726697f4ef27
Alex Chung <alexcssh27@gmail.com>
2025-07-13 19:45:21 -0700

    last

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,15 +1,26 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function Watch() {
-    const videoPrefix = 'https://storage.googleapis.com/alexcsh-yt-processed-videos/'
-    const videoSrc = useSearchParams().get('v');
+function WatchInner() {
+  const videoPrefix = 'https://storage.googleapis.com/alexcsh-yt-processed-videos/';
+  const videoSrc = useSearchParams().get('v');
 
-    return (
-        <div>
-            <h1>Watch Page!</h1>
-            <video controls src={videoPrefix + videoSrc} />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Watch Page</h1>
+      <video controls src={videoPrefix + videoSrc} />
+    </div>
+  );
+}
+
+export const dynamic = 'force-dynamic';
+
+export default function WatchPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchInner />
+    </Suspense>
+  );
 }
